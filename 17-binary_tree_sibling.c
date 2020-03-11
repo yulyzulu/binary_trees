530265066979597de6080a2f7e7565c6ee24bee0
yulyzulu <yulyzulu05@gmail.com>
2020-03-11 20:37:35 +0000

    Added find the sibling of a node task

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+* binary_tree_sibling- Function that finds the sibling of a node
+*@node: Node to be check the sibling from
+*Return: The sibling node value
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *s;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	else if (node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
+
+	else if (node->parent->left == node)
+		s = node->parent->right;
+
+	else
+		s = node->parent->left;
+	return (s);
+}
